c660f663267a53f4c124ce79c83d93b3a4f4821c
fungkinchun <fungkinchun@gmail.com>
2026-02-22 20:11:07 +0000

    generated by spring initializr

---
+++ src/main/kotlin/com/fancia/backend/shared/common/SharedCommonApplication.kt
@@ -0,0 +1,11 @@
+package com.fancia.backend.shared.common
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharedCommonApplication
+
+fun main(args: Array<String>) {
+	runApplication<SharedCommonApplication>(*args)
+}
