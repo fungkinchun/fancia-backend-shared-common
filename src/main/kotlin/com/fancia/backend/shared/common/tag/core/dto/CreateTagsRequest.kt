67aed35461d997f29f94bce013e9d9ca308a129d
fungkinchun <fungkinchun@gmail.com>
2026-06-18 00:06:26 +0100

    first commit

--- src/main/kotlin/com/fancia/backend/shared/common/tag/core/dto/CreateTagsRequest.kt
+++ src/main/kotlin/com/fancia/backend/shared/common/tag/core/dto/CreateTagsRequest.kt
@@ -1,24 +1,8 @@
 package com.fancia.backend.shared.common.tag.core.dto
 
-import com.fancia.backend.shared.common.tag.core.enums.TagType
 import jakarta.validation.Valid
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
-import jakarta.validation.constraints.Size
 
 data class CreateTagsRequest(
     @field:Valid
-    val tags: List<@Valid SingleTagCreation> = emptyList(),
-)
-
-data class SingleTagCreation(
-    @field:NotBlank(message = "Tag name is required")
-    @field:Size(
-        min = 2,
-        max = 50,
-        message = "Tag name must be between 2 and 50 characters",
-    )
-    val name: String,
-    @field:NotNull(message = "Tag type is required")
-    val type: TagType = TagType.INTEREST,
+    val tags: List<@Valid TagItemRequest> = emptyList(),
 )
